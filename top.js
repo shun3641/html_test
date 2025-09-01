eb2e6bc346f6e3b2df412d023773a1aacc1eb3d3
shun3641 <tanakaLearning15q>
2025-09-01 13:58:58 +0900

    変更しました

--- top.js
+++ top.js
@@ -3,16 +3,19 @@
   $(function () {
     const navBtn = $(".navBtn")
     navBtn.on('click', function () {
+      $(".navBtn").toggleClass("active");
       $(".navBtnTxt").toggleClass("active");
       $('.navSp').toggleClass("active");
     });
 
     $(".navItem").on('click', function () {
+      $(".navBtn").toggleClass("active");
       $(".navSp").toggleClass("active")
       setTimeout(function () {
-        $(".navSpBtn").toggleClass("active")
-        $(".navSpArrow").toggleClass("active")
+        $(".navSpBtn").toggleClass("active");
       }, 800);
-    });
+
+        $(".navBtnTxt").toggleClass("active");
+      });
   })
 }
