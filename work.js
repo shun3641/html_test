d72c03395cddc0275209f5e40205e8c6da2b4b2b
shun3641 <tanakaLearning15q>
2025-09-09 10:34:45 +0900

    変更しました

--- work.js
+++ work.js
@@ -8,19 +8,15 @@ const params = new URLSearchParams(queryString);
 const id = params.get("id");
 
 const myPortfolio = {
-  title: "Amuse",
+  title: "myPortfolio",
   url: "",
   Image: "ipad-myportfolio.png",
   //検証画面でデバイスサイズの幅を1500×3452に設定したまま
   // capture full Size Screenshotで撮影した画像を使用した
   fullSiteUrl: [
-    "amuse/amuse_page1.png",
-    "amuse/amuse_page2.png",
-    "amuse/amuse_page3.png",
-    "amuse/amuse_page4.png",
-    "amuse/amuse_page5.png",
-    "amuse/amuse_page6.png",
-    "amuse/amuse_top.png",
+    "myPortfolio/myPortfolio_top.png",
+    "myPortfolio/myPortfolio_page1.png",
+    "myPortfolio/myPortfolio_page2.png",
   ],
   contents: 
     {
@@ -29,7 +25,7 @@ const myPortfolio = {
       designProcess: 
       "カスタムHTMLを一切使わないことで誰でも編集できるサイトを心掛けました。" +
       "<br>使用者の混乱を防ぐため、ヘッダーは常時追従するように設計しました。",
-      heardThing: "画像のサイズ調整に苦戦しました。" + 
+      hardThing: "画像のサイズ調整に苦戦しました。" + 
       "<br>上手くデフォルトCSSを上書きすることで何とか違和感なく持っていきました。" +
       "<br>wordpressのjqueryの文法に癖があるため、そこを動かすのに苦戦してました。",
       usedTool: "wordpress(lightning)/ css(カスタムcssプラグイン使用)/ jQuery(カスタムJSプラグイン使用)/ illustrator",
@@ -60,7 +56,7 @@ const amuse = {
       designProcess: 
       "カスタムHTMLを一切使わないことで誰でも編集できるサイトを心掛けました。" +
       "<br>使用者の混乱を防ぐため、ヘッダーは常時追従するように設計しました。",
-      heardThing: "画像のサイズ調整に苦戦しました。" + 
+      hardThing: "画像のサイズ調整に苦戦しました。" + 
       "<br>上手くデフォルトCSSを上書きすることで何とか違和感なく持っていきました。" +
       "<br>wordpressのjqueryの文法に癖があるため、そこを動かすのに苦戦してました。",
       usedTool: "wordpress(lightning)/ css(カスタムcssプラグイン使用)/ jQuery(カスタムJSプラグイン使用)/ illustrator",
@@ -77,16 +73,17 @@ const desertRand = {
   fullSiteUrl: ["desertRand/desertRand_top.png"],
     contents: 
     {
-      target: "10~50代の家族連れ、カップル。思い出作りをしたい方々",
-      siteImage: "爽快さが伝わるように鮮やかな写真と青を基調とした配色を心掛けました。",
+      target: "スイーツが好きな10~20代の男女お腹いっぱい食べたい人向け",
+      siteImage: "JK風のポップな可愛らしいデザイン",
       designProcess: 
-      "カスタムHTMLを一切使わないことで誰でも編集できるサイトを心掛けました。" +
-      "<br>使用者の混乱を防ぐため、ヘッダーは常時追従するように設計しました。",
-      heardThing: "画像のサイズ調整に苦戦しました。" + 
-      "<br>上手くデフォルトCSSを上書きすることで何とか違和感なく持っていきました。" +
-      "<br>wordpressのjqueryの文法に癖があるため、そこを動かすのに苦戦してました。",
-      usedTool: "wordpress(lightning)/ css(カスタムcssプラグイン使用)/ jquery(カスタムJSプラグイン使用)/ illustrator",
-      period: "1か月"
+      "全体をサーモンピンクでまとめ、アクセントにオレンジを絡め、カラフルに設計しました。" +
+      "<br>利用する人が困らないように営業日、よくある質問など設置しました。" +
+      "葉っぱやスイカでサイトの個性を演出しました。",
+      hardThing: "始めの1スクロールで画像拡大のところでかなり悩みました。" + 
+      "<br>トップページのイメージ画像のサイズ調整が初めてなのでかなり試行錯誤してた。" + 
+      "<br>カーソルに追従するキラキラエフェクトはAIに書いてもらいました。",
+      usedTool: "HTML/ CSS/ javascript(jquery)/ photoshop",
+      period: "3週間"
     }
 
 }
@@ -114,11 +111,11 @@ const activeFit = {
       designProcess: 
       "カスタムHTMLを一切使わないことで誰でも編集できるサイトを心掛けました。" +
       "<br>使用者の混乱を防ぐため、ヘッダーは常時追従するように設計しました。",
-      heardThing: "画像のサイズ調整に苦戦しました。" + 
+      hardThing: "画像のサイズ調整に苦戦しました。" + 
       "<br>上手くデフォルトCSSを上書きすることで何とか違和感なく持っていきました。" +
       "<br>wordpressのjqueryの文法に癖があるため、そこを動かすのに苦戦してました。",
-      "usedTool": "wordpress(lightning)/ css(カスタムcssプラグイン使用)/ jquery(カスタムJSプラグイン使用)/ illustrator",
-      "period": "1か月"
+      "usedTool": "",
+      "period": ""
     }
 
 }
@@ -137,7 +134,7 @@ $(".urlText").text(SiteInfo[id].url);
 $("#target").html(SiteInfo[id].contents.target);
 $("#siteImage").html(SiteInfo[id].contents.siteImage);
 $("#designProcess").html(SiteInfo[id].contents.designProcess);
-$("#heardThing").html(SiteInfo[id].contents.heardThing);
+$("#hardThing").html(SiteInfo[id].contents.hardThing);
 $("#usedTool").text(SiteInfo[id].contents["usedTool"]);
 $("#period").text(SiteInfo[id].contents.period);
 console.log(SiteInfo[id].fullSiteUrl);
